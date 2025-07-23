157559bdea255f77305baafcbfd70b65d853a41c
65saeedz <65saeedz@gmail.com>
2025-07-23 11:18:44 +0330

    counter app react first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,44 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
+export default function App() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View style={styles.body}>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.appBar}>
+          <Text style={styles.title}>flutter default app with react</Text>
+        </View>
+      </SafeAreaView>
+      <View style={styles.content}>
+        <Text>محتوای اصلی برنامه</Text>
+      </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: "#2196F3",
+  },
+  appBar: {
+    height: 56,
+    backgroundColor: "#2196F3",
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 4,
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "#ff1f",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  body: {
+    flex: 1,
+    backgroundColor: "#f2ff",
+  },
+});
